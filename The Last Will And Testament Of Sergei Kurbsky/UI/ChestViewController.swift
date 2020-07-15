10cc9ed2fb6f0121d75334d18808e10cea862a1a
immersedreality <jeffreyeugenehoch@gmail.com>
2020-07-15 02:12:51 -0400

    locks one through six working

--- The Last Will And Testament Of Sergei Kurbsky/UI/ChestViewController.swift
+++ The Last Will And Testament Of Sergei Kurbsky/UI/ChestViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import CoreMotion
+import AVFoundation
 
 class ChestViewController: UIViewController {
     
@@ -24,8 +26,15 @@ class ChestViewController: UIViewController {
     
     var allLocks: [LockImageView] = []
 
+    var lockThreeIsEnabled: Bool = false
+    let motionManager = CMMotionManager()
+    let audioSession = AVAudioSession.sharedInstance()
+    var appTimer = Timer()
+
     override func viewDidLoad() {
         configureLocks()
+        startProcessingGyroEvents()
+        activateAudioSession()
     }
 
     private func configureLocks() {
@@ -39,6 +48,7 @@ class ChestViewController: UIViewController {
         allLocks.append(lockEightImageView)
         allLocks.append(lockNineImageView)
         allLocks.append(lockTenImageView)
+        allLocks.forEach { $0.configureSoundEffects() }
     }
 
     private func checkIfChestShouldOpen() {
@@ -57,7 +67,7 @@ class ChestViewController: UIViewController {
     }
 
     private func openChest() {
-        
+
     }
 
     @IBAction func presentLetterButtonTapped(_ sender: Any) {
@@ -65,29 +75,142 @@ class ChestViewController: UIViewController {
 
 }
 
-//LockOne
+//LocksOneAndTwo
 extension ChestViewController {
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        guard let touch = touches.first else { return }
-        if touch.view == chestImageView {
-            lockOneImageView.unlock()
+        if touches.contains(where: { (touch) -> Bool in
+            touch.view == chestImageView
+        }) {
+            if lockOneImageView.checkIfUnlocked() == false {
+                lockOneImageView.unlock()
+            }
+            checkIfChestShouldOpen()
+            let pinchGestureRecognizer = UIPinchGestureRecognizer(target: self, action: #selector(unlockLockTwo))
+            chestImageView.addGestureRecognizer(pinchGestureRecognizer)
         }
     }
 
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        guard let touch = touches.first else { return }
-        if touch.view == chestImageView {
+        chestImageView.gestureRecognizers?.removeAll()
+        lockThreeIsEnabled = false
+        if lockOneImageView.checkIfUnlocked() == true {
             lockOneImageView.lock()
+        }
+
+        if lockTwoImageView.checkIfUnlocked() == true {
+            lockTwoImageView.lock()
+        }
+
+        if lockThreeImageView.checkIfUnlocked() == true {
+            lockThreeImageView.lock()
         }
     }
 
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        guard let touch = touches.first else { return }
-        let touchLocation = touch.location(in: chestImageView)
-        if chestImageView.bounds.contains(touchLocation) == false {
-            lockOneImageView.lock()
+        if touches.contains(where: { (touch) -> Bool in
+            let touchLocation = touch.location(in: chestImageView)
+            return chestImageView.bounds.contains(touchLocation) == false
+        }) {
+            chestImageView.gestureRecognizers?.removeAll()
+            lockThreeIsEnabled = false
+
+            if lockOneImageView.checkIfUnlocked() == true {
+                lockOneImageView.lock()
+            }
+
+            if lockTwoImageView.checkIfUnlocked() == true {
+                lockTwoImageView.lock()
+            }
+
+            if lockThreeImageView.checkIfUnlocked() == true {
+                lockThreeImageView.lock()
+            }
         }
+    }
+
+    @objc func unlockLockTwo() {
+        if lockTwoImageView.checkIfUnlocked() == false {
+            lockTwoImageView.unlock()
+            lockThreeIsEnabled = true
+            checkIfChestShouldOpen()
+        }
+    }
+
+}
+
+//LockThree
+extension ChestViewController {
+
+    override func motionBegan(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        guard lockThreeIsEnabled else { return }
+        if motion == .motionShake {
+            if lockThreeImageView.checkIfUnlocked() == false {
+                lockThreeImageView.unlock()
+                checkIfChestShouldOpen()
+            }
+        }
+    }
+
+}
+
+//LocksFourFiveAndSix {
+extension ChestViewController: AVAudioRecorderDelegate {
+
+    private func startProcessingGyroEvents() {
+        if motionManager.isGyroAvailable && motionManager.isAccelerometerAvailable {
+            motionManager.gyroUpdateInterval = 1.0/60.0
+            motionManager.startGyroUpdates()
+            motionManager.accelerometerUpdateInterval = 1.0/60.0
+            motionManager.startAccelerometerUpdates()
+
+            appTimer = Timer(fire: Date(), interval: (1.0/60.0), repeats: true, block: { (timer) in
+                if let data = self.motionManager.gyroData {
+                    if data.rotationRate.z < -2 {
+                        if self.lockFourImageView.checkIfUnlocked() == false {
+                            self.lockFourImageView.unlock()
+                            self.checkIfChestShouldOpen()
+                        }
+                    } else {
+                        if self.lockFourImageView.checkIfUnlocked() == true {
+                            self.lockFourImageView.lock()
+                        }
+                    }
+                }
+
+                if let data = self.motionManager.accelerometerData {
+                    if data.acceleration.z > 0 {
+                        if self.lockFiveImageView.checkIfUnlocked() == false {
+                            self.lockFiveImageView.unlock()
+                            self.checkIfChestShouldOpen()
+                        }
+                    } else {
+                        if self.lockFiveImageView.checkIfUnlocked() == true {
+                            self.lockFiveImageView.lock()
+                        }
+                    }
+                }
+
+                if self.audioSession.outputVolume < 0.80 && self.audioSession.outputVolume > 0.70  {
+                    if self.lockSixImageView.checkIfUnlocked() == false {
+                        self.lockSixImageView.unlock()
+                        self.checkIfChestShouldOpen()
+                    }
+                } else {
+                    if self.lockSixImageView.checkIfUnlocked() == true {
+                        self.lockSixImageView.lock()
+                    }
+                }
+
+            })
+
+            RunLoop.current.add(self.appTimer, forMode: RunLoop.Mode.default)
+        }
+
+    }
+
+    private func activateAudioSession() {
+        try? audioSession.setActive(true)
     }
 
 }
--- The Last Will And Testament Of Sergei Kurbsky/UI/LockImageView.swift
+++ The Last Will And Testament Of Sergei Kurbsky/UI/LockImageView.swift
@@ -7,21 +7,36 @@
 //
 
 import UIKit
+import AVFoundation
 
 class LockImageView: UIImageView {
 
     private var isUnlocked = false
+    private var lockSound: AVAudioPlayer?
+    private var unlockSound: AVAudioPlayer?
+
+    func configureSoundEffects() {
+        let lockPath = Bundle.main.path(forResource: "Lock.flac", ofType: nil) ?? ""
+        let lockURL = URL(fileURLWithPath: lockPath)
+        lockSound = try? AVAudioPlayer(contentsOf: lockURL)
+
+        let unlockPath = Bundle.main.path(forResource: "Unlock.wav", ofType: nil) ?? ""
+        let unlockURL = URL(fileURLWithPath: unlockPath)
+        unlockSound = try? AVAudioPlayer(contentsOf: unlockURL)
+    }
 
     func lock() {
         image = UIImage(systemName: "lock.fill")
         tintColor = UIColor.red
         isUnlocked = false
+        lockSound?.play()
     }
 
     func unlock() {
         image = UIImage(systemName: "lock.open.fill")
         tintColor = UIColor.green
         isUnlocked = true
+        unlockSound?.play()
     }
 
     func checkIfUnlocked() -> Bool {
